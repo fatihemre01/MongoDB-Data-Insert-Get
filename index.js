df8e8484609839e7e909e78eff0e94898e7b3911
Fatih Emre <108624615+fatihemre01@users.noreply.github.com>
2022-08-09 20:02:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,37 @@
-//Import mongoose and connecting to mongodb
 const mongoose = require("mongoose")
-mongoose.connect("mongodb://localhost/myDatabase")
-    .then(() => console.log("Connected!"))
-    .catch(err => console.log(err))
+mongoose.connect("mongodb://localhost/myDb")
+    .then(() => console.log("Connected"))
 
-//Creating a schema
-const courseSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
     name: String,
-    price: Number,
-    quantity: Number,
-    isPublished: Boolean
+    age: Number
 })
 
-//Creating a model
-let Course = mongoose.model("Course", courseSchema)
+const User = mongoose.model("User", userSchema)
 
-let courses = [
-    { name: "JavaScript Tutorial", price: 10, quantity: 25, isPublished: true },
-    { name: "Nodejs Tutorial", price: 15, quantity: 5, isPublished: false },
-    { name: "Mongodb Tutorial", price: 20, quantity: 2, isPublished: true },
+let users = [
+    { "name": "John", "age": 18},
+    { "name": "Kevin", "age": 25}
 ]
 
-
-function insertCourses() {
-    Course.collection.insertMany(courses)
+async function createUser() {
+    const result = await User.create(users)
+    console.log(result)
 }
 
-async function getCourses() {
-    return await Course
-        .find({isPublished: true})
-        .sort({name: 1})
-        .select({name: 1, _id: 0})
+async function getUser() {
+    const result = await User
+        .find()
+        .or([{name: "John"}, {age: 25}])
+    console.log(result)
 }
 
-getCourses().then(x => console.log(x))
+async function updateUser() {
+    const result = await User.updateOne({name: "Kevin"}, {age: 12})
+    console.log(result)
+}
 
-//insertCourses()
+async function deleteUser() {
+    const result = await User.deleteOne({name: "Kevin"})
+    console.log(result)
+}
